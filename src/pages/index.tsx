3bf891575b1e13ddb4ff7aab4d68553aec3e2dc8
hai <hai@entest.io>
2022-04-24 06:23:36 +0000

    update hoempage with theming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,14 @@ import {
   Image,
   View,
   Heading,
+  Authenticator,
+  IconChevronRight,
+  ToggleButtonGroup,
+  ToggleButton,
 } from '@aws-amplify/ui-react';
 import { HomeLogo } from './../components/HomeLogo';
 import { Copy } from '../components/Copy';
+import { ThemeButton } from './ThemeButton';
 import * as React from 'react';
 
 const MyHomePage = () => {
@@ -97,6 +102,85 @@ const MyHomePage = () => {
         <Heading level={2} textAlign="center" margin={tokens.space.large}>
           Take it for a test drive
         </Heading>
+      </View>
+
+      <View as="section" className="docs-home-section container">
+        <Flex direction={{ base: 'column-reverse', large: 'row' }}>
+          <View flex={1} className="example">
+            <Authenticator
+              socialProviders={['amazon', 'apple', 'facebook', 'google']}
+            ></Authenticator>
+          </View>
+          <Flex flex={1} direction="column" alignItems={'flex-start'}>
+            <Heading level={2}>Cloud-Connected Components</Heading>
+            <Text className="docs-home-description">
+              Simplify complex cloud-connected workflows like authentication
+              with minimal bolerplate code.
+            </Text>
+            <Button as="a" size="large" isFullWidth>
+              Authenticator
+              <IconChevronRight></IconChevronRight>
+            </Button>
+          </Flex>
+        </Flex>
+      </View>
+
+      <View>
+        <Flex
+          className="container"
+          direction={{ base: 'column', large: 'row' }}
+        >
+          <Flex flex={1} direction="column" alignItems={'flex-start'}>
+            <Heading level={2}>Themeing</Heading>
+            <Text className="docs-home-description">
+              Themeing capabilities that allow you to customize the appearance
+              of Amplify UI to match your brand.
+            </Text>
+            <Button as="a" size="large">
+              Get started with themeing
+              <IconChevronRight></IconChevronRight>
+            </Button>
+          </Flex>
+          <Flex
+            flex={1}
+            alignContent="center"
+            padding={{ base: '0', large: tokens.space.large }}
+          >
+            <ToggleButtonGroup
+              width={'100%'}
+              value={'system'}
+              isExclusive
+              onChange={(value: string) => {
+                console.log(value);
+              }}
+            >
+              <ToggleButton value="" flex={1}>
+                <ThemeButton
+                  label={'Default'}
+                  colors={[
+                    'var(--amplify-colors-teal-60)',
+                    'var(--amplify-colors-purple-60)',
+                  ]}
+                ></ThemeButton>
+              </ToggleButton>
+              <ToggleButton value="" flex={'1'}>
+                <ThemeButton
+                  label={'Terminal'}
+                  colors={['#44AF5G', '#000']}
+                ></ThemeButton>
+              </ToggleButton>
+              <ToggleButton value="classic" flex={'1'}>
+                <ThemeButton
+                  label={'Classic'}
+                  colors={[
+                    'var(--amplify-colors-blue-60)',
+                    'var(--amplify-colors-neutral-60)',
+                  ]}
+                ></ThemeButton>
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Flex>
+        </Flex>
       </View>
     </div>
   );
